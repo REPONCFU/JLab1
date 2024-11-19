2055584854f0750daa41aa18baeaecbd78356cf3
REPONCFU <stavbortsov@mail.ru>
2024-11-19 12:34:54 +0300

    Add main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # ИИ-комик
 def main():
     print("Привет! Я ИИ-комик. Задай мне вопрос или дай команду, связанную с ML или ИИ!")
+    user_input = input("Ты: ")
+    respond(user_input)
+
+def respond(command):
+    # Будет реализовано позже
+    pass
 
 if __name__ == "__main__":
     main()
